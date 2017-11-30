a4034a7625994d410ef385b183822a89221f7afc
Enmanuel Reyes <EnmanuelReyes@live.com>
2017-11-30 18:40:53 -0400

    Finishing Section 16

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyClCKvBd9TgurW1IcXlzY-HA4N8BhGQtXg',
+      authDomain: 'manager-99c5b.firebaseapp.com',
+      databaseURL: 'https://manager-99c5b.firebaseio.com',
+      projectId: 'manager-99c5b',
+      storageBucket: '',
+      messagingSenderId: '275574811486'
+    };
+  firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+
+export default App;
